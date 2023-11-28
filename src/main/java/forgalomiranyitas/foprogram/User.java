c809d2bae0b6b30b9985cdd2455ac451d43c9a50
SteveDMC <dmc120@gmail.com>
2023-11-28 15:30:04 +0100

    Regisztráció frissítés

--- src/main/java/forgalomiranyitas/foprogram/User.java
+++ src/main/java/forgalomiranyitas/foprogram/User.java
@@ -1,8 +1,6 @@
 package forgalomiranyitas.foprogram;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
